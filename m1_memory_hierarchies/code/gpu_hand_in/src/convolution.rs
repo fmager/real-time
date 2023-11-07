7827ff8e152a039af747d81278cda3636a410789
Anders Bo Sørensen <anders-soerensen@hotmail.com>
2023-11-07 10:32:58 +0100

    Add functions for testing the correctness of the CPU implementations

--- m1_memory_hierarchies/code/gpu_hand_in/src/convolution.rs
+++ m1_memory_hierarchies/code/gpu_hand_in/src/convolution.rs
@@ -7,8 +7,8 @@ fn convolution_cpu(signal: &Vec<f32>, filter: &Vec<f32>) -> Vec<f32> {
     for signal_index in 0..signal.len() {
         for filter_index in 0..filter.len() {
             let offset_signal_index: i64 = signal_index as i64 - filter_offset as i64 + filter_index as i64;
-            if 0 < offset_signal_index && offset_signal_index < signal.len() as i64 {
-                output[signal_index] = signal[offset_signal_index as usize] * filter[filter_index];
+            if -1 < offset_signal_index && offset_signal_index < signal.len() as i64 {
+                output[signal_index] += signal[offset_signal_index as usize] * filter[filter_index];
             }
         }
     }
@@ -16,9 +16,34 @@ fn convolution_cpu(signal: &Vec<f32>, filter: &Vec<f32>) -> Vec<f32> {
     output
 }
 
+fn test_ground_truth() -> bool {
+    let signal: Vec<f32> = vec![ 1.0, 1.0, 1.0, 1.0, 1.0];
+    let filter: Vec<f32> = vec![ 0.25, 0.5, -0.25];
+    let ground_truth_output: Vec<f32> = vec![ 0.25, 0.5, 0.5, 0.5, 0.75];
+
+    let output: Vec<f32> = convolution_cpu(&signal, &filter);
+
+    assert!(output.len() == ground_truth_output.len());
+    for index in 0..ground_truth_output.len() {
+        if 0.00001 < (output[index] - ground_truth_output[index]).abs() {
+            println!("Provided output: {:?}", output);
+            println!("Ground truth output: {:?}", ground_truth_output);
+            return false;
+        }
+    }
+
+    true
+}
+
 pub fn convolution(handles: &GPUHandles) -> bool {
-    let data_element_count: usize = 100;
-    let filter_size: usize = 5;
+    // A small test to ensure that the convolution_cpu function is actually correct.
+    let ground_truth_is_correct: bool = test_ground_truth();
+    println!("Convolution ground truth function is correct: {}", ground_truth_is_correct);
+    assert!(ground_truth_is_correct);
+
+
+    let data_element_count: usize = 1000000;
+    let filter_size: usize = 19;
     let signal: Vec<f32> = (0..data_element_count).map(|x| x as f32 * 0.1).collect();
     let filter: Vec<f32> = (0..filter_size).map(|x| x as f32 * 0.1).collect();
 
@@ -28,11 +53,13 @@ pub fn convolution(handles: &GPUHandles) -> bool {
     //
     // YOUR CODE HERE
     // Make one version of 1D convolution on the GPU which uses if's to guard against the filter
-    // Make another version uses a zero padded version of the original signal to not use any if's
+    // Make another version using a zero padded version of the original signal to not use any if's
     // inside the inner for-loop. This zero padding is (filter_size - 1) / 2 on each side of the
-    // signal. Both versions should use shared memory. See what is the fastest, is it the signal
-    // in shared memory, is it the filter in shared memory, is it both?
-    //
+    // signal.
+    // Both versions should use shared memory.
+    // See which is the fastest, is it the signal in shared memory, is it the filter in
+    // shared memory, is it both?
+    // Make sure to keep the filter and signal large enough to offset the cost of data transfer
 
     println!("convolution MSE: {}", mean_square_error(&ground_truth, &dummy_data));
     let success: bool = are_vectors_equivalent(&ground_truth, &dummy_data);
--- m1_memory_hierarchies/code/gpu_hand_in/src/matrix_multiplication.rs
+++ m1_memory_hierarchies/code/gpu_hand_in/src/matrix_multiplication.rs
@@ -20,22 +20,51 @@ fn matrix_multiplication_cpu(
     output
 }
 
+fn test_ground_truth() -> bool {
+    let outer_dimension_left: usize = 4;
+    let inner_dimension: usize = 3;
+    let outer_dimension_right: usize = 3;
+    let left: Vec<f32> = vec![ 1.0, 0.0, 1.0, 2.0, 1.0, 1.0, 0.0, 1.0, 1.0, 1.0, 1.0, 2.0];
+    let right: Vec<f32> = vec![ 1.0, 2.0, 1.0, 2.0, 3.0, 1.0, 4.0, 2.0, 2.0];
+    let ground_truth_output: Vec<f32> = vec![ 5.0, 4.0, 3.0, 8.0, 9.0, 5.0, 6.0, 5.0, 3.0, 11.0, 9.0, 6.0];
+
+    let output: Vec<f32> = matrix_multiplication_cpu(&left, &right, outer_dimension_left, inner_dimension, outer_dimension_right);
+
+    assert!(output.len() == ground_truth_output.len());
+    for index in 0..ground_truth_output.len() {
+        if 0.00001 < (output[index] - ground_truth_output[index]).abs() {
+            return false;
+        }
+    }
+
+    true
+}
+
 pub fn matrix_multiplication(handles: &GPUHandles) -> bool {
-    let outer_dimension_left: usize = 40; // M
-    let inner_dimension: usize = 32; // N
-    let outer_dimension_right: usize = 54;// K
+    // A small test to ensure that the matrix_multiplication_cpu function is actually correct.
+    let ground_truth_is_correct: bool = test_ground_truth();
+    println!("Matrix multiplication ground truth function is correct: {}", ground_truth_is_correct);
+    assert!(ground_truth_is_correct);
+
+
+    // Use big data dimensions to make sure the cost of transferring
+    // doesn't dominate the time spent in the function.
+    let outer_dimension_left: usize = 400; // M
+    let inner_dimension: usize = 320; // N
+    let outer_dimension_right: usize = 540;// K
     let left_matrix: Vec<f32> = (0..outer_dimension_left*inner_dimension).map(|x| x as f32 * 0.1).collect();
     let right_matrix: Vec<f32> = (0..inner_dimension*outer_dimension_right).map(|x| x as f32 * 0.1).collect();
-
     let ground_truth: Vec<f32> = matrix_multiplication_cpu(&left_matrix, &right_matrix, outer_dimension_left, inner_dimension, outer_dimension_right);
     let dummy_data: Vec<f32> = ground_truth.clone();
     
     //
     // YOUR CODE HERE
-    // Make one version of matrix multiplication, ensure that it is correct
+    // Make one version of matrix multiplication using the GPU. Ensure that it is correct.
     // Make another version which uses tiling through shared memory and local accumulation in
-    // a register. How big do the matrices have to be before you see a big performance
-    // difference? Why do you think that is?
+    // a register. A tiling reference: http://www.csce.uark.edu/~mqhuang/courses/4643/s2016/lecture/GPU_Lecture_3.pdf
+    // After ensuring correctness - time the two functions.
+    // How big do the matrices have to be before you see a big performance
+    // difference?
 
     println!("matrix_multiplication MSE: {}", mean_square_error(&ground_truth, &dummy_data));
     let success: bool = are_vectors_equivalent(&ground_truth, &dummy_data);
